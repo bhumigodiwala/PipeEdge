0a9981dd1d53d708f739de2f4455e3a838090165
Haonan Wang <haonanwa@usc.edu>
2022-09-17 17:11:33 -0700

    experiments for RML test

--- evaluation.py
+++ evaluation.py
@@ -1,15 +1,100 @@
 """ Evaluate accuracy on ImageNet dataset of PipeEdge """
 import os
 import argparse
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor, Normalize, Resize, Compose
 from transformers import DeiTFeatureExtractor, ViTFeatureExtractor
-from pipeedge.quantization.hook import forward_hook_quant_encode, forward_hook_quant_encode_with_clamp, forward_pre_hook_quant_decode
-from pipeedge.quantization.clamp_op import clamp
+from src.pipeedge.quantization.hook import forward_hook_quant_encode, forward_hook_quant_encode_with_clamp, forward_pre_hook_quant_decode
+from src.pipeedge.quantization.clamp_op import clamp_factor
 from runtime import _get_default_quant
 import model_cfg
+
+PLOT_BINS = 200
+
+def normalized(a, axis=-1, order=1):
+    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
+    l2[l2==0] = 1
+    return (a / np.expand_dims(l2, axis)).flatten()
+
+def mse(ori_x,x, is_norm=False):
+    if isinstance(ori_x, torch.Tensor):
+        ori_x = ori_x.numpy()
+    if isinstance(x, torch.Tensor):
+        x = x.numpy()
+    mse = (np.square(ori_x-x)).mean()
+    if is_norm:
+        mse = mse/((ori_x)**2).mean()
+    return mse
+
+def Laplace(x, mu, beta, bin_len):
+    return np.exp(-np.abs(x-mu)/beta)/(2*beta)
+
+def Gaussian(x, mu, sigma, bin_len):
+    return np.exp(-0.5*np.power((x-mu)/sigma, 2))*bin_len/(sigma*2.50662827463)
+
+def estimate_alpha(x):
+    return np.power(x, 2).sum()/x.size
+
+def estimate_beta(x):
+    return np.abs(x).sum()/x.size
+
+def RML_test(x):
+    alpha = estimate_alpha(x)
+    beta = estimate_beta(x)
+    T = np.log(np.sqrt(alpha)) - np.log(beta) - 0.27421
+    return T, (alpha, beta)
+
+def fitting_with_search(norm_hist, theo_xaxi, fitting_parameter, fitting_type = 'laplace', search_range = 0.4, search_steps=100):
+    if fitting_type == 'laplace':
+        fitting_curve = Laplace(theo_xaxi, 0.0, fitting_parameter, (theo_xaxi.max()-theo_xaxi.min())/PLOT_BINS)
+    else:
+        fitting_curve = Gaussian(theo_xaxi, 0.0, np.sqrt(fitting_parameter), (theo_xaxi.max()-theo_xaxi.min())/PLOT_BINS)
+    norm_fitting_curve = normalized(fitting_curve)
+    search_direction = -1.0 if norm_hist.max() > norm_fitting_curve.max() else 1.0
+
+    step_len = search_range * fitting_parameter / search_steps
+    best_parameter, best_mse = 0.0, 10000.0
+    for i in range(search_steps+1):
+        temp_parameter = fitting_parameter + i*step_len*search_direction
+        if fitting_type == 'laplace':
+            fitting_hist = Laplace(theo_xaxi, 0.0, temp_parameter, (theo_xaxi.max()-theo_xaxi.min())/PLOT_BINS)
+        else:
+            fitting_hist = Gaussian(theo_xaxi, 0.0, np.sqrt(temp_parameter), (theo_xaxi.max()-theo_xaxi.min())/PLOT_BINS)
+        fitting_hist = normalized(fitting_hist)
+        temp_mse = mse(norm_hist, fitting_hist)
+        if best_mse > temp_mse:
+            best_mse = temp_mse
+            best_parameter = temp_parameter
+    return best_parameter
+
+
+def clamp_with_minimalMSE(input, bit, layer_type='normal'):
+    input_numpy = input.numpy()
+    hist_x, bins_x = np.histogram(input_numpy, bins = PLOT_BINS)
+    theo_xaxi = np.linspace(bins_x.min(), bins_x.max(), PLOT_BINS)
+    norm_hist_x = normalized(hist_x)
+    rml, (alpha_hat, beta_hat) = RML_test(input_numpy)
+    # near-neighbor search
+    if rml>0:
+        #Laplace
+        fitting_type = 'laplace'
+        fitting_parameter = beta_hat
+    else:
+        #Gaussain
+        fitting_type = 'gaussian'
+        fitting_parameter = alpha_hat
+    # best_parameter = fitting_with_search(norm_hist_x, theo_xaxi, fitting_parameter, fitting_type=fitting_type)
+    # optimal_clamp_range = clamp_factor[layer_type][fitting_type][bit] * best_parameter
+    optimal_clamp_range = clamp_factor[layer_type][fitting_type][bit] * fitting_parameter
+    optimal_clamp_range = torch.tensor(optimal_clamp_range).float()
+
+    # clamp
+    result = torch.where(torch.abs(input)<optimal_clamp_range, input, optimal_clamp_range)
+
+    return result, optimal_clamp_range
 
 class ReportAccuracy():
     def __init__(self, batch_size, output_dir, model_name, partition, quant) -> None:
@@ -107,14 +192,14 @@ def _forward_model(input_tensor, model_shards, stage_layers, is_clamp=True, mode
                 temp_clamp_list = []
                 if len(temp_tensor) == 2:
                     for tensor_idx, tensor in enumerate(temp_tensor):
-                        result_tensor, clamp_value = clamp(tensor, shard.quant_bits[1].item(), layer_type=clamp_types[tensor_idx])
+                        result_tensor, clamp_value = clamp_with_minimalMSE(tensor, shard.quant_bits[1].item(), layer_type=clamp_types[tensor_idx])
                         temp_tensors_list.append(result_tensor)
                         temp_clamp_list.append(clamp_value)
                     temp_tensor = tuple(temp_tensors_list)
                 elif len(temp_tensor) == 1:
                     if isinstance(temp_tensor, tuple):
                         temp_tensor = temp_tensor[0]
-                    temp_tensor, clamp_value = clamp(temp_tensor, shard.quant_bits[1].item(), layer_type=clamp_types[0])
+                    temp_tensor, clamp_value = clamp_with_minimalMSE(temp_tensor, shard.quant_bits[1].item(), layer_type=clamp_types[0])
                     temp_clamp_list.append(clamp_value)
                     temp_tensor = (temp_tensor,)
                 # do encoder
@@ -204,7 +289,7 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--batch-size", default=1, type=int, help="batch size")
     parser.add_argument("-q", "--quant", type=str,
                         help="comma-delimited list of quantization bits to use after each stage")
-    parser.add_argument("-pt", "--partition", type=str, default= '1,43,44,48',
+    parser.add_argument("-pt", "--partition", type=str, default= '1,22,23,48',
                         help="comma-delimited list of start/end layer pairs, e.g.: '1,24,25,48'; "
                              "single-node default: all layers in the model")
     parser.add_argument("-o", "--output-dir", type=str, default="/home1/haonanwa/projects/PipeEdge/results")
